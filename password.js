1f7e664f68924f8d34d04ebb32efb75df7060755
Nicholas Anderson <anderson.nick@me.com>
2022-03-04 08:29:36 -0800

    completed assessment and extra credit

--- password.js
+++ password.js
@@ -10,11 +10,33 @@ const reader = readline.createInterface({
 console.log(`Welcome to the password verification tool!`)
 
 reader.question("What is your proposed password?", function(input){
-    if (input.length >= 10){
-        console.log(`Your password meets the length requirement`)
+   
+    var numberCheck = new RegExp(/\d/)
+
+    if (input.length >= 10 && numberCheck.exec(input) != null){
+        console.log(`Your password meets the length and number requirements!\nBeam me up Scotty`)
+        console.log(`         ___________________        ____....-----....____
+        (________________LL_)   ==============================
+            ______\\   \\_______.--'.  \`---..._____...---'
+            \`-------..__            \` ,/
+                        \`-._ -  -  - |
+                            \`-------'
+       `)     
+    } else if (input.length >= 10){
+        console.log(`Your password must include a number. Please try again`)
+        console.log(`──────▄▀▄─────▄▀▄
+─────▄█░░▀▀▀▀▀░░█▄
+─▄▄──█░░░░░░░░░░░█──▄▄
+█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█
+        `)
     } else {
         console.log(`Your password must be at least 10 characters long. Please try again`)
+        console.log(`───▄▄▄
+─▄▀░▄░▀▄
+─█░█▄▀░█
+─█░▀▄▄▀█▄█▄▀
+▄▄█▄▄▄▄███▀`)
     }
 
     reader.close()
-  }
+});
